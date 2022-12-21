ded814443dee81a58ca147f79e8b599f375ae8dd
B4rn4rd0 <103604854+B4rn4rd0@users.noreply.github.com>
2022-12-21 15:50:02 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+img = "";
+status = "";
 
 function preload(){
     img = loadImage('ps5.jpg');
@@ -6,6 +8,21 @@ function preload(){
 function setup(){
     canvas = createCanvas(640, 420);
     canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detectando Objetos";
+}
+
+function modelLoaded(){
+    console.log("Modelo Carregado!")
+    status = true;
+    objectDetector.detect(img, gotResults);
+}
+
+function gotResults(error, results) {
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
 }
 
 function draw(){
@@ -14,5 +31,11 @@ function draw(){
     text("Playstation 5", 45, 75);
     noFill();
     stroke(255, 0, 0);
-    rect(30, 15, 520, 380)
+    rect(30, 15, 520, 380);
+
+    fill(255, 0, 0);
+    text("Controle", 55, 235);
+    noFill();
+    stroke(255, 0, 0);
+    rect(30, 210, 300, 180)
 }
